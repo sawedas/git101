ab4055c50385746630c3ccc1997852f08dcc002a
Tar_Tachit <Tachit200@gmail.com>
2024-02-02 12:54:34 +0700

    update code-02.cpp on branch main

--- code-02.cpp
+++ code-02.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-    cout << "Hello Git in c++" << endl; //std::cout cout == console output // endl : end line '\n'
+    cout << "Love C++" << endl; //std::cout cout == console output // endl : end line '\n'
     return 0;
 }
